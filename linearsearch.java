564d338e2efbf70156e363bb6725aa07ab08e298
sonaliAy <sonalijaiswals2005@gmail.com>
2026-06-27 13:48:33 +0530

    day-2

---
+++ linearsearch.java
@@ -0,0 +1,22 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        int target = 30;
+        int index = -1;
+        //LINEAR SEARCH:-
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                index = i;
+                break;
+            }
+        }
+        if(index != -1){
+            System.out.println("Element found at index :" +index);
+        }
+        else{
+            System.out.println("Element not found");
+        }
+    }
+
+    
+}
